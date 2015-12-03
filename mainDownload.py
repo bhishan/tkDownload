551fb34fdc1766a182da2ef6ea9555da7f9b7131
bhishan <bhishan_0306@deerwalk.edu.np>
2015-12-03 21:01:04 +0545

    Fixed issue with RETURN event not working which now calls the download method

--- mainDownload.py
+++ mainDownload.py
@@ -4,6 +4,10 @@ import os
 from easygui import msgbox
 class Window(Frame):
     def __init__(self, master=None):
+        self.root = Tk()
+        self.root.title("Downloader")
+        self.root.geometry("450x95")
+        self.root.resizable(0, 0)
         Frame.__init__(self, master)
         self.init_window()
 
@@ -19,13 +23,15 @@ class Window(Frame):
         self.url = Entry(self, relief=FLAT, textvariable=display, justify='left',bd=2,bg='white',width=45)
         self.url.grid(row=1,column=2,sticky=W,padx=2,pady=2)
 
+        self.root.bind('<Return>', self.startDownload)
+
         self.downloadButton = Button(self,text="Download",command=self.startDownload)
         self.downloadButton.grid(row=2,column=2,sticky=NSEW,padx=2,pady=2)
 
         self.quitButton = Button(self,text="Quit",command=lambda: self.quit())
         self.quitButton.grid(row=3,column=2,sticky=E,padx=2,pady=2)
 
-    def startDownload(self):
+    def startDownload(self, event):
         vUrl = self.url.get()
         if vUrl != "" and vUrl.startswith("https://youtube.com"):
             command = "youtube-dl "+vUrl
@@ -36,11 +42,10 @@ class Window(Frame):
     # def quitApp(self):
     #     exit()
 
+    def start(self):
+        self.root.mainloop()
+
 
 if __name__ == "__main__":
-    root = Tk()
-    root.title("Downloader")
-    root.geometry("450x95")
-    root.resizable(0, 0)
-    app = Window(root)
-    root.mainloop()
+   Window().start()
+
